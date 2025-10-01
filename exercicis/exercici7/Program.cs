b33647739da9bff06a6f25b99c428d24632e1e36
lluis <llucas@cendrassos.net>
2025-10-01 22:44:45 +0200

    ex 7

--- exercicis/exercici7/Program.cs
+++ exercicis/exercici7/Program.cs
@@ -7,6 +7,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Que has tret a catala?");
+        string nota_catala = Console.ReadLine();
+
+        int catala = Convert.ToInt32(nota_catala);
+
+        Console.WriteLine("Que has tret a mates?");
+        string nota_mates = Console.ReadLine();
+
+        int mates = Convert.ToInt32(nota_mates);
+
+        Console.WriteLine("Que has tret a fisica?");
+        string nota_fisica= Console.ReadLine();
+
+        int fisica = Convert.ToInt32(nota_fisica);
+
+        int mitja = (catala + mates + fisica) / 3;
+
+        Console.WriteLine($"la nota mitja es de {mitja}");
     }
 }
