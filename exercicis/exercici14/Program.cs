88d68d53e8f37ce70b549f329bd4f9bad8d0f532
lluis <llucas@cendrassos.net>
2025-10-03 16:45:25 +0200

    fds

--- exercicis/exercici14/Program.cs
+++ exercicis/exercici14/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace exercici14;
 
 // Declara una paraula i fes que el programa imprimeixi només la primera lletra, l'última lletra i la lletra del mig 
+/* 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, ");
     }
+}*/
+class Program
+{
+    static void Main(string[] args);
+
+
+
+    Console.WriteLine("digues una paraula");
+    
+    string paraula = Console.ReadLine();
+
+    primeralletra = paraula.Length[0]
+    mitjalletra = paraula.Length / 2
+
+        ultimalletra = paraula.Length-1
+
+        Console.WriteLine($ "Les lletres son {primeralletra} + {mitjalletra} + {ultimalletra} ");
 }
